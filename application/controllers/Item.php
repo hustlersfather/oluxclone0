56854c5d64e5956eb3ed751e1afd4228e684222e
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2025-02-27 03:35:04 +0100

    Update Item.php

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -1,63 +1,73 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Item extends CI_Controller {
+namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Models\Cpanel;
+use App\Models\Rdp;
+use App\Models\Ssh;
+use App\Models\Mailer;
+use App\Models\Shell;
+use App\Models\Smtp;
+use App\Models\Lead;
 
-  public function __construct()
-  {
-    parent::__construct();
-    $this->load->model('cpanel');
-    $this->load->model('rdp');
-    $this->load->model('ssh');
-    $this->load->model('mailer');
-    $this->load->model('shell');
-    $this->load->model('smtp');
-    $this->load->model('lead');
-  }
-  
-  public function list($type = 'rdp')
-  {
-    $whitelist = array('rdp', 'cpanel', 'shell', 'ssh', 'mailer', 'smtp', 'lead');
-    
-    if(!in_array($type, $whitelist))
-      show_404();
-    
-    $this->template->displayHeader();
-    
-    $data = $this->{$type}->getList();
-
-    $item_template = '';
-	
-
-    foreach($data as $key => $item)
+class ItemController extends Controller
+{
+    /**
+     * Display a list of items for the given type.
+     *
+     * @param  string  $type
+     * @return \Illuminate\View\View
+     */
+    public function list($type = 'rdp')
     {
-      if(isset($item->https)) {
-        if($item->https == 1) {
-          $this->template->assign('lock', 'fa-lock');
-          $this->template->assign('https', 'https');
-          $this->template->assign('color', '#18BC9C');
-        } else {
-          $this->template->assign('lock', 'fa-lock-open');
-          $this->template->assign('https', 'http');
+        // Define allowed types.
+        $whitelist = ['rdp', 'cpanel', 'shell', 'ssh', 'mailer', 'smtp', 'lead'];
+        if (!in_array($type, $whitelist)) {
+            abort(404);
         }
-      }
-      
-      $webmail = isset($item->webmail) && $item->webmail == 0 ? 'No' : 'Yes';
-      $this->template->assign('webmail', $webmail);
-      
-      $whm = isset($item->whm) && $item->whm == 0 ? 'No' : 'Yes';
-      $this->template->assign('whm', $whm);
-      
-      $this->template->assignObject($item, ['https', 'webmail', 'whm']);
-      $item_template = $this->template->load_template($type . '_item');
-    }
 
-    
-    $this->template->assign('items', $item_template);
-    $this->template->display(strtolower($type));
-    
-    $this->template->displayFooter();
-  }
-  
+        // Map each type to its corresponding model class.
+        $models = [
+            'rdp'    => Rdp::class,
+            'cpanel' => Cpanel::class,
+            'shell'  => Shell::class,
+            'ssh'    => Ssh::class,
+            'mailer' => Mailer::class,
+            'smtp'   => Smtp::class,
+            'lead'   => Lead::class,
+        ];
+
+        $modelClass = $models[$type];
+
+        // Retrieve list of items using the model’s getList() method.
+        $data = $modelClass::getList();
+
+        // Process each item.
+        $items = collect($data)->map(function ($item) {
+            // Process HTTPS settings.
+            if (isset($item->https)) {
+                if ($item->https == 1) {
+                    $item->lock  = 'fa-lock';
+                    $item->https = 'https';
+                    $item->color = '#18BC9C';
+                } else {
+                    $item->lock  = 'fa-lock-open';
+                    $item->https = 'http';
+                }
+            }
+
+            // Determine webmail and WHM availability.
+            $item->webmail = (isset($item->webmail) && $item->webmail == 0) ? 'No' : 'Yes';
+            $item->whm     = (isset($item->whm) && $item->whm == 0) ? 'No' : 'Yes';
+
+            return $item;
+        });
+
+        // Render the view for this type.
+        // For example, the view file might be at resources/views/items/rdp.blade.php.
+        return view('items.' . strtolower($type), [
+            'items' => $items,
+        ]);
+    }
 }
